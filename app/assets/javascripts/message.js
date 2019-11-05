2fc88ae976867192e3b3f84b7ab251f12db76ac5
hot-and-cool <hisato0217take@gmail.com>
2019-11-05 14:19:24 +0900

    formにresetメソッド適用

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,8 +39,7 @@ $(function(){
     .done(function(message){
       var html = buildMessage(message);
       $('.messages').append(html);
-      $('.form__message').val('');
-      $('.hidden').val('');
+      $('.new_message')[0].reset();
       $('.form__submit').prop('disabled', false);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
     })
